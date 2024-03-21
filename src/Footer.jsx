0d9fd7872a8b57e6bd2ec5711d3511563ba189a3
Nikhil Gangaramani <nikhil.gangaramani@gmail.com>
2024-03-20 21:35:39 -0400

    first-commit

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Footer(){
+    return(
+        <footer>
+           <small> @ 2024 Gangaramani development. All Rights reserved.</small>
+        </footer>
+    )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Header(){
+    return(
+    <header>
+            <nav className="nav">
+                    <img src="./react-logo.png" className="nav-logo"></img>
+                    <ul className="nav-items">
+                        <li>Pricing</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+            </nav>
+    </header>
+    )
+}
